0d5ce2382c4883993dba4a19e4f34de43fe99f84
RafaelMcMarques <mcmarquesrafa@gmail.com>
2026-05-19 10:38:56 +0200

    Skeleton + Basic Optimizer

---
+++ src_to_implement/Layers/__init__.py
@@ -0,0 +1,17 @@
+__all__ = ["Helpers", "FullyConnected", "SoftMax", "ReLU", "Base"]
+
+class FullyConnected:
+    def FullyConnected(self):
+        pass
+
+class SoftMax:
+    def SoftMax(self):
+        pass
+
+class ReLU:
+    def ReLU(self):
+        pass
+
+class Base: 
+    def base(self):
+        pass
