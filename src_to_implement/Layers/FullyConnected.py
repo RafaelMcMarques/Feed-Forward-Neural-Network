0df6bfdbf439cce0a0dce4ef6651a6357e60d930
RafaelMcMarques <mcmarquesrafa@gmail.com>
2026-05-20 15:35:44 +0200

    Tidy-up fully connected layer

--- src_to_implement/Layers/FullyConnected.py
+++ src_to_implement/Layers/FullyConnected.py
@@ -1,8 +1,6 @@
 from .Base import BaseLayer
 import numpy as np
 
-#TODO: this currently calculates Y = X @ W, change to Y = W.t @ X
-# also, change optimizer to be a @property instead of attribute
 class FullyConnected(BaseLayer):
     optimizer = None
 
@@ -13,24 +11,26 @@ class FullyConnected(BaseLayer):
         self.optimizer = optimizer
 
     def __init__ (self, input_size, output_size):
+        # call super here
+        super().__init__()
         self.input_size = input_size
         self.output_size = output_size
         self.trainable = True
         self.weights = np.random.rand(input_size + 1, output_size)
 
     def forward(self, input_tensor):
-        self.input = np.column_stack((input_tensor, np.ones(input_tensor.shape[0]))) 
-        self.output = self.input @ self.weights
+        self.input = np.column_stack((input_tensor, np.ones(input_tensor.shape[0]))) # input was (m x input), now (m x input + 1)
+        self.output = self.input @ self.weights # (m x output) = (m x input+1) @ (input + 1, output)
         return self.output
     
     def backward(self, error_tensor):
         #dL/dW = dL/dy * dy/dW
-        self.gradient_weights = self.input.T @ error_tensor
+        self.gradient_weights = self.input.T @ error_tensor # (input + 1 x output) = (input + 1 x m) @ (m x output)
 
         #dL/dx = dL/dy * dy/dx
-        prev_error_tensor = error_tensor @ self.weights[:-1, :].T
+        prev_error_tensor = error_tensor @ self.weights[:-1, :].T # (m x input) = (m x output) @ (output x input)
 
         if self.optimizer:
             self.weights = self.optimizer.calculate_update(self.weights, self.gradient_weights)
 
-        return prev_error_tensor 
+        return prev_error_tensor # this is dL/dx, which has dimensions m x input
